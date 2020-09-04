994509c2f8baf0fbd2eb43d906e659ad2342e9b1
Nick Pascucci <npascut1@gmail.com>
2020-09-04 18:03:29 +0100

    Start hooking up the CRDTs to the UI.

--- src/Main.elm
+++ src/Main.elm
@@ -3,8 +3,13 @@ module Main exposing (..)
 import Browser
 import Crdt exposing (..)
 import Dict exposing (Dict)
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Events exposing (..)
+import Element.Font as Font
+import Element.Input exposing (button)
+import Html exposing (Html, div)
+import Maybe exposing (withDefault)
 import Set exposing (Set)
 
 
@@ -45,22 +50,89 @@ init =
 
 
 type Msg
-    = Increment
-    | Decrement
+    = NewCard
 
 
+
+-- TODO: Add UUID generation logic, then wire up the NewCard action so that it adds the UUID to the
+-- "To Discuss" column state and a default CardContents to the contents state.
+
+-- TODO: On change to the model, dispatch CRDT updates to websockets.
 update msg model =
     case msg of
-        Increment ->
+        NewCard ->
             model
 
-        Decrement ->
-            model
+
+type Column
+    = ToDiscuss
+    | InProgress
+    | Done
+
+
+getColumn column boardState =
+    let
+        ( td, ip, dn ) =
+            boardState
+    in
+    case column of
+        ToDiscuss ->
+            td
+
+        InProgress ->
+            ip
+
+        Done ->
+            dn
+
+
+cardTitles column model =
+    let
+        boardState =
+            withDefault ( [], [], [] ) <| latest model.positions
+
+        colIds =
+            getColumn column boardState
+
+        contents =
+            model.contents
+
+        titles =
+            Dict.map
+                (\k v ->
+                    v
+                        |> latest
+                        |> withDefault ( "Untitled", "Ignored" )
+                        |> Tuple.first
+                )
+                contents
+    in
+    List.map
+        (\k ->
+            case Dict.get k titles of
+                Nothing ->
+                    text "Untitled"
+
+                Just t ->
+                    text t
+        )
+        colIds
 
 
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text "Hello, world" ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
+    layout [] <|
+        row [ width fill, height fill ]
+            [ column
+                [ width <| fillPortion 1
+                , Background.color <| rgb255 92 99 118
+                , Font.color <| rgb255 255 255 255
+                ]
+              <|
+                [ el [ Font.underline ] <| text "To Discuss" ]
+                    ++ cardTitles ToDiscuss model
+                    ++ [ button [ Font.size 12, Font.italic ]
+                            { label = text "+ New Card", onPress = Just NewCard }
+                       ]
+            , column [ width <| fillPortion 1 ] [ el [ Font.underline ] <| text "In Progress" ]
+            , column [ width <| fillPortion 1 ] [ el [ Font.underline ] <| text "Done" ]
+            ]
